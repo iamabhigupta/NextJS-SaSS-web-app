3f1536af560832aa47d3c86da3a92a1e524e5749
jawed7siddiqui@gmail.com <jawed7siddiqui@gmail.com>
2022-09-06 18:16:00 +0530

    codebase connect to production api & cors issues resolved for localhost

--- next.config.js
+++ next.config.js
@@ -27,3 +27,11 @@ module.exports = withTM({
     return config
   }
 })
+
+// module.exports = {
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+// }
